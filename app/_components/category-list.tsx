2c31b3921491ee2fd63219235d36c6c9792d7e1b
Leonardo Ortiz de Souza <leonardoortizbr@hotmail.com>
2024-05-14 18:10:06 -0300

    feat: add initial list components

---
+++ app/_components/category-list.tsx
@@ -0,0 +1,18 @@
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
+
+    const categories = await db.category.findMany({})
+    // Pegar as categorias do banco de dados
+    // Renderizar um item para cada categoria
+    return ( 
+        <div className="flex overflow-scroll">
+        {categories.map(category => (
+            <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
+     );
+}
+ 
+export default CategoryList;
