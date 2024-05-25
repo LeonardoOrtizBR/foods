acac1ff29787b463538002dcda4a85ee975e4040
Leonardo Ortiz de Souza <leonardoortizbr@hotmail.com>
2024-05-24 21:08:24 -0300

    feat: Link in logo on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
-    return ( 
+    return (
         <div className="flex justify-between pt-6 px-5">
-            <Image src="/logo.svg" alt="FSW food" height={30} width={100} />
+            <Link href='/'>
+                <Image src="/logo.svg" alt="FSW food" height={30} width={100} />
+            </Link>
             <Button size="icon" variant="outline" className="bg-transparent border-none">
-                <MenuIcon/>
+                <MenuIcon />
             </Button>
         </div>
-     );
+    );
 }
- 
+
 export default Header;
